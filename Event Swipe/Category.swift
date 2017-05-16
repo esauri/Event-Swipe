26334761de7fdba0e9dbc6c8d3536819c728e0d2
Erick <ericksaurisyl@gmail.com>
2017-05-16 02:42:11 -0400

    added start screen

---
+++ Event Swipe/Category.swift
@@ -0,0 +1,11 @@
+//
+//  Categories.swift
+//  Event Swipe
+//
+//  Created by Erick Sauri on 5/15/17.
+//  Copyright © 2017 Erick Sauri. All rights reserved.
+//
+
+import Foundation
+
+cl
---
+++ Event Swipe/OnboardingCategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingCategoryCell.swift
+//  Event Swipe
+//
+//  Created by Erick Sauri on 5/15/17.
+//  Copyright © 2017 Erick Sauri. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingCategoryCell: UICollectionViewCell {
+    
+}
